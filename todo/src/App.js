ca0f44a2e7cab9258b9760042013cb9bd16af42b
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-28 17:55:32 -0600

    components updated

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <TodoList />
-      </div>
-    )
-  }
+import TodoList from './components/todoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
